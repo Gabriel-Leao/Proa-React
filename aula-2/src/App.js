05983018bc735d039ab35441cc9866a81f1310bf
Gabriel-Leao <gabriellsilva1200@outlook.com.br>
2022-04-26 13:03:01 -0300

    aula 2

---
+++ aula-2/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+     <h1>Hello World</h1>
+     <p>Harry Styles é horrível</p>
+
+     <h2>Meu plano de morrer dormindo não deu certo, Bom dia!</h2>
+     <p>Como pode o salpicão, um alimento tão gostoso, ser feito apenas de sal e de uma pica grande.</p>
+    </div>
+  );
+}
+
+export default App;
